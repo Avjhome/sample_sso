d737af0110bf0914b475e218e55c2afc40a63d01
hyunwoo.kwon <hyunwoo.kwon@vtw.co.kr>
2022-07-19 13:22:24 +0900

    20220719_1322_KHW 1.create project

---
+++ src/main/java/com/example/samplesso/SampleSsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplesso/SampleSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
