11825610be11937337116c4396c37e0f9d201017
hyunwoo.kwon <hyunwoo.kwon@vtw.co.kr>
2022-07-22 18:53:35 +0900

    20220722_1815_KHW 1.spring security 설정 기본테스트

--- src/main/java/sample_sso/spring/SecurityConfigure.java
+++ src/main/java/sample_sso/spring/SecurityConfigure.java
@@ -1,11 +1,27 @@
 package sample_sso.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigure {
 
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.httpBasic();
+
+        http.authorizeRequests()
+//                .antMatchers("/security").permitAll()  //permitAll 해당하는 하위경로 모두 인증받지않고 사용가능
+                .antMatchers("/").authenticated();
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+
 }
